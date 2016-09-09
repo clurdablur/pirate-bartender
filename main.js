11d4476b052ab64db14f171e5b18a48c1c341a3b
Claire Coulter <coucc15@gmail.com>
2016-09-09 13:19:32 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,70 +1,47 @@
 $(document).ready(function() {
-	$('#submitButton').click(function() {
+	var Bartender = function() {
+		this.answers = null;
+		this.answerquestion = function(userAnswers) {
+			this.answers = userAnswers
+		}
+	};
+	Bartender.prototype.makeDrink = function(selectArray) {
+		for (i = 0; i < selectArray.length; i++) {
+			var select = $(selectArray[i]);
+			if (select.val() === 'yes') {
+				var name = select.attr('name');
+				var randomValue = getRandom();
+				var ingredientArray = [];
+				ingredientArray.push(ingredients[name][randomValue]);
 
-		
-
-
-		Bartender.prototype.makeDrink = function(selectArray) {
-			for (i = 0; i < selectArray.length; i++) {
-				var select = $(selectArray[i]);
-				if (select.val() === 'yes') {
-					var name = select.attr('name');
-					var randomValue = getRandom();
-					var ingredientArray = [];
-					ingredientArray.push(ingredients[name][randomValue]);
-
-					var html = '<h3>'+ingredientArray+'</h3>'
-					$('#ingredient-results').append(html);
-				}
+				var html = '<h3>'+ingredientArray+'</h3>'
+				$('#ingredient-results').append(html);
 			}
 		}
-
-		
-
+	}
+	var myBartender = new Bartender();
+	$('#submitButton').click(function() {
+		myBartender.makeDrink($('selectArray'));
 	})
 
-var questions = {'strong': 'Do ye like your drinks strong?',
-'salty': 'do ye like it with a salty tang?',
-'bitter': 'are ye a lubber who likes it bitter?',
-'sweet': 'would ye like a bit of sweetness with yer posion?',
-'fruity': 'are ye one for a fruity finish?'};
+	var questions = {'strong': 'Do ye like your drinks strong?',
+	'salty': 'do ye like it with a salty tang?',
+	'bitter': 'are ye a lubber who likes it bitter?',
+	'sweet': 'would ye like a bit of sweetness with yer posion?',
+	'fruity': 'are ye one for a fruity finish?'};
 
-var ingredients = {'strong': ['gulg of rum', 'slug of whiskey', 'splash of gin'],
-'salty': ['olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
-'bitter': ['shake of bitters', 'splash of tonic', 'twist of lemon peel'],
-'sweet': ['sugar cube', 'spoonful of honey', 'splash of colar'],
-'fruity': ['slice of orange', 'dash of cassis', 'cherry on top']};
+	var ingredients = {'strong': ['gulg of rum', 'slug of whiskey', 'splash of gin'],
+	'salty': ['olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
+	'bitter': ['shake of bitters', 'splash of tonic', 'twist of lemon peel'],
+	'sweet': ['sugar cube', 'spoonful of honey', 'splash of colar'],
+	'fruity': ['slice of orange', 'dash of cassis', 'cherry on top']};
 
-
-
-var Client = function(name, preferences) {
-	this.name = name
-	this.preferences = preferences
-}
-
-var Bartender = function() {
-	this.answers = null;
-	this.answerquestion = function(userAnswers) {
-		this.answers = userAnswers
+	function getRandom(){
+		return Math.floor(Math.random() * (2 - 0 + 1) + 0);
 	}
-}
 
-
-
-
-
-var myBartender = new Bartender();
-var test = new Bartender();
-
-
-function getRandom(){
-	return Math.floor(Math.random() * (2 - 0 + 1) + 0);
-}
-
-$('#typesubmit').submit(function(event){
-	event.preventDefault();
-	myBartender.makeDrink($('select'));
-}) 
+	$('#typesubmit').submit(function(event){
+		event.preventDefault();
+		myBartender.makeDrink($('select'));
+	}) 
 })
-
-
